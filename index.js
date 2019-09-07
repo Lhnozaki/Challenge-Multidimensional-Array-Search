b583e10c2106cf33bac55862355e36cb42c1b5c9
Lewis Nozaki <lhnozaki@gmail.com>
2019-09-07 10:47:23 -1000

    changed code

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function locate(array, target) {
   // do work here
-  return array
-    .join("")
-    .split("")
-    .includes(target);
+  let arr = [];
+
+  let pushIntoArr = element => {
+    element.forEach(e => {
+      if (typeof e == "object") {
+        pushIntoArr(e);
+        return;
+      } else {
+        arr.push(e);
+      }
+    });
+    return arr;
+  };
+  return pushIntoArr(array).includes(target);
 }
 
 module.exports = locate;
